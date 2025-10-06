beec36472f49962a32eecebf41474392bad29917
neeun <neeunbox@gmail.com>
2025-10-06 14:55:02 +0900

    BaseCartService modify

--- gallery/src/main/java/kr/co/sinamu/gallery/cart/repository/CartRepository.java
+++ gallery/src/main/java/kr/co/sinamu/gallery/cart/repository/CartRepository.java
@@ -4,6 +4,7 @@ import kr.co.sinamu.gallery.cart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
@@ -11,7 +12,7 @@ public interface CartRepository extends JpaRepository<Cart, Integer> {
     List<Cart> findAllByMemberId(Integer memberId);
 
     // 장바구니 정보 조회(특정 회원 및 특정 상품)
-    List<Cart> findByMemberIdAndItemId(Integer memberId, Integer itemId);
+    Optional<Cart> findByMemberIdAndItemId(Integer memberId, Integer itemId);
 
     // 장바구니 삭제(특정 회원)
     void deleteByMemberId(Integer memberId);
