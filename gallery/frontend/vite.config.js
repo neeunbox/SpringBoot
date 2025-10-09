69652aeb529688df8bfc8df72953d4846ff5470c
neeun <neeunbox@gmail.com>
2025-10-09 18:57:34 +0900

    vite.config.js add

--- gallery/frontend/vite.config.js
+++ gallery/frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+      proxy: {
+          "/v1/api" : {
+              target: "http://localhost:8080",
+              changeOrigin: true
+          }
+      }
+  },
   plugins: [
     vue(),
   ],
@@ -13,4 +20,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
