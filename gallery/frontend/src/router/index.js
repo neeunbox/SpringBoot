d150621c5da53db45b946606d5336256562490a6
neeun <neeunbox@gmail.com>
2025-10-09 20:22:37 +0900

    Join.vue add

--- gallery/frontend/src/router/index.js
+++ gallery/frontend/src/router/index.js
@@ -10,8 +10,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Home.vue')
-    }
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: () => import('../views/Join.vue')
+    },
   ]
-})
+});
 
 export default router
