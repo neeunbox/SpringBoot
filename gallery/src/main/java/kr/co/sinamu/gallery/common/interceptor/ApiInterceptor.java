78422c65f94b4f1aa863c9916946729358c76c27
neeun <neeunbox@gmail.com>
2025-11-02 14:05:48 +0900

    modify

--- gallery/src/main/java/kr/co/sinamu/gallery/common/interceptor/ApiInterceptor.java
+++ gallery/src/main/java/kr/co/sinamu/gallery/common/interceptor/ApiInterceptor.java
@@ -1,4 +1,27 @@
 package kr.co.sinamu.gallery.common.interceptor;
 
-public class ApiInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.sinamu.gallery.account.helper.AccountHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class ApiInterceptor implements HandlerInterceptor {
+
+    private final AccountHelper accountHelper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) {
+
+        // 로그인 회원 아이디가 없으면
+        if (accountHelper.getMemberId(req) == null) {
+            res.setStatus(401);
+            return false;
+        }
+
+        return true;
+    }
 }
